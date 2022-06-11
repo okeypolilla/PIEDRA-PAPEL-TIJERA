b1fe7347d2160ddd6fc98de8f13cc663afca15e7
okeypolilla <jotogt27@gmail.com>
2022-06-10 21:14:43 -0300

    ultimo update

---
+++ index.js
@@ -0,0 +1,95 @@
+var http=require('http');
+var url=require('url');
+var fs=require('fs');
+var querystring = require('querystring');
+
+const mime = {
+  'html': 'text/html',
+  'css': 'text/css',
+  'jpg': 'image/jpg',
+  'ico': 'image/x-icon',
+  'mp3': 'audio/mpeg3',
+  'mp4': 'video/mp4'
+}
+
+const servidor = http.createServer((pedido, respuesta) => {
+  const url = new URL('http://localhost:8888' + pedido.url)
+  let camino = 'public' + url.pathname
+  if (camino == 'public/')
+    camino = 'public/index.html'
+  encaminar(pedido, respuesta, camino)
+})
+
+servidor.listen(8888)
+
+
+function encaminar(pedido, respuesta, camino) {
+  console.log(camino)
+  switch (camino) {
+    case 'public/recuperardatos': {
+      recuperar(pedido, respuesta)
+      break
+    }
+    default: {
+      fs.stat(camino, error => {
+        if (!error) {
+          fs.readFile(camino, (error, contenido) => {
+            if (error) {
+              respuesta.writeHead(500, { 'Content-Type': 'text/plain' })
+              respuesta.write('Error interno')
+              respuesta.end()
+            } else {
+              const vec = camino.split('.')
+              const extension = vec[vec.length - 1]
+              const mimearchivo = mime[extension]
+              respuesta.writeHead(200, { 'Content-Type': mimearchivo })
+              respuesta.write(contenido)
+              respuesta.end()
+            }
+          })
+        } else {
+          respuesta.writeHead(404, { 'Content-Type': 'text/html' })
+          respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>')
+          respuesta.end()
+        }
+      })
+    }
+  }
+}
+
+
+function recuperar(pedido, respuesta) {
+  let info = ''
+  pedido.on('data', datosparciales => {
+    info += datosparciales
+  })
+  pedido.on('end', () => {
+    const formulario = new URLSearchParams(info)
+    console.log(formulario)
+    respuesta.writeHead(200, { 'Content-Type': 'text/html' })
+    const pagina =
+      `<!doctype html>
+      <html>
+      <head>
+      <link rel="stylesheet" type="text/css" href="styles/css/style.css">
+      </head>
+      <body>
+      <div class="box">
+      <div>
+      <img src="${formulario.get('op')}"   >
+      </div
+     Nombre de usuario:${formulario.get('op')}<br>
+     
+     <div aling="center">
+     <a href="index.html">JUGAR DE NUEVO</a>
+     </div>
+    
+     </div>
+     
+     </body></html>`
+    respuesta.end(pagina)
+  })
+}
+
+
+console.log('Servidor web iniciado')
